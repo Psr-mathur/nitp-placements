19391a676af3335e85a338e4be9b34e74ab0290a
Prakash Kumar <Prakashkumarggh@gmail.com>
2023-04-08 13:18:17 +0530

    re-fixed Route and Link

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ export default function App(){
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/nitp-placements" element={<Home/>} />
-                <Route path="/nitp-placements/formfill" element={<Form/>}/>
-                <Route path="/nitp-placements/error" element={<h1>Error Page Not Found</h1>}/>
+                <Route exact path="/nitp-placements" element={<Home/>} />
+                <Route path="/formfill" element={<Form/>}/>
+                <Route path="*" element={<h1>Error Page Not Found</h1>}/>
             </Routes>
         </BrowserRouter>
     )
