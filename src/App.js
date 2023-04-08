fad34b3818f829b0144a6002190ccf2ef16e68fe
Prakash Kumar <Prakashkumarggh@gmail.com>
2023-04-09 01:22:55 +0530

    HashRouter fixed the issue of target->blank

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Home from "./Components/Home";
-import { BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter,Routes,Route,HashRouter} from "react-router-dom";
 import Form from "./Components/Form";
 
 
 
 export default function App(){
     return (
-        <BrowserRouter basename="/nitp-placements">
+        <HashRouter>
             <Routes>
-                <Route exact path="/" element={<Home/>} />
+                <Route path="/" element={<Home/>}/>
                 <Route path="/formfill" element={<Form/>}/>
-                <Route path="*" element={<h1>Error Page Not Found</h1>}/>
+                <Route path="/*" element={<h1>Error Page Not Found</h1>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
-}
+};
